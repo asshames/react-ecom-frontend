5b2c8328359ca536b38ef82795b18f53b50b7d2f
asshames <assamialomarshames@gmail.com>
2023-02-20 19:10:00 +0600

    fixing bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import Products from './containers/Products/Products';
-import Navigations from './component/Navigations/Navigations';
-import classes from './App.css';
+import Navigations from './component/Navigation/NavigationItems/NavigationItems';
+import classes from './App.module.css';
 
 const App =()=>{
   return(
-    <div className="classes.App">
+    <div className={classes.App}>
       <Navigations/>
       <Products/>
     </div>
